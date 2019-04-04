5461b46d7fbe02e8507f4c1dc267f4938b413a0e
Jonathan <jonathan.lelowski@gmail.com>
2019-04-04 12:10:57 +0200

    Version3

--- boucleTableau.py
+++ boucleTableau.py
@@ -8,11 +8,33 @@ Tableau de valeurs et Boucle
 monTableau = [15,3,25,12,7,-15]
 
 #Simple poor loop
-For indice, val in enumerate(monTableau):
-    if indice MOD 2 = 0
-        Print(monTbaleau[indice] x 2)
+for indice, val in enumerate(monTableau):
+        print(monTableau[indice] * 2)
 
 #More complex loop with condition
-For indice, val in enumerate(monTableau):
-    if indice % 2 == 0
-    Print(monTbaleau[indice] x 2)
+for indice, val in enumerate(monTableau):
+    if indice % 2 == 0:
+        print("Les valeurs paires sont les suivantes :", monTableau[indice] * 2)
+
+#Déterminer la valeur la plus petite
+valeurMinimale = monTableau[0]
+for val in monTableau[1:]:
+        if  monTableau[indice] < valeurMinimale :
+                valeurMinimale = monTableau[indice]
+print("La valeur minimale est :", valeurMinimale)
+
+
+#Déterminer la valeur la plus grande
+valeurMaximale = monTableau[0]
+for val in monTableau[1:]:
+        if  monTableau[indice] > valeurMaximale :
+                valeurMaximale = monTableau[indice]
+print("La valeur max est :", valeurMaximale)
+
+#Déterminer la valeur la plus petite (fonction min)
+valeurMin = min(monTableau)
+print("La valeur min est :", valeurMin)
+
+#Déterminer la valeur la plus grande (fonction max)
+valeurMax = max(monTableau)
+print("La valeur max est :", valeurMax)
