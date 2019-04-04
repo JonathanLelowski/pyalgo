3ed83f86d3c756f1d2f023697eea73f9cd54ace7
Jonathan <jonathan.lelowski@gmail.com>
2019-04-04 10:24:06 +0200

    Firstversion

--- boucleTableau.py
+++ boucleTableau.py
@@ -2,9 +2,9 @@
 Tableau de valeurs et Boucle
 """
 
-#Var
+#Déclaration du tableau de démonstration
 monTableau = [15,3,25,12,7,-15]
 
-For indice = in range (0, 5) inc1
+For indice, val in enumerate(monTableau)
     if indice MOD 2 = 0
         Print(monTbaleau[indice] x 2)
